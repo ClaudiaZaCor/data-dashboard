a63df2e6619a005cf65b8416605c497796522c77
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2017-11-29 01:27:50 -0500

    funcion de botones alumnas y estadisticas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
-/*
- * Funcionalidad de tu producto
- */
+var mostrarOcultar = function(e) {
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+  var tabSeleccionado = e.target.dataset.tabSeleccionado;
+
+  var estadisticas = document.querySelector('#estadisticas');
+  var alumnas = document.querySelector('#alumnas');
+
+  if (tabSeleccionado === 'tabEstadisticas') {
+    // ocultar alumnas
+    alumnas.style.display = 'none';
+    // mostrar estadisticas
+    estadisticas.style.display = 'block';
+  } else if (tabSeleccionado === 'tabAlumnas') {
+    console.log('vamos a almorzar');
+    // ocultar estadisticas
+    estadisticas.style.display = 'none';
+    // mostrar alumnas
+    alumnas.style.display = 'block';
+  }
+};
+
+var cargarPagina = function() {
+  var estadisticas = document.querySelector('#estadisticas');
+  var alumnas = document.querySelector('#alumnas');
+
+  estadisticas.style.display = 'none';
+  alumnas.style.display = 'none';
+
+  var elementosTab = document.getElementsByClassName('tab');
+  for (var i = 0; i < elementosTab.length; i++) {
+    elementosTab[i].addEventListener('click', mostrarOcultar);
+  }
+};
+
+cargarPagina();
